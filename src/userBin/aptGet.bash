2430dd64463251a02249d53c151e5129956d660c
Jaid <jaid.jsx@gmail.com>
2023-01-04 13:44:13 +0100

    Added userBin aptGet
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/userBin/aptGet.bash
@@ -0,0 +1,1 @@
+DEBIAN_FRONTEND=noninteractive apt-get --option Acquire::Retries=60 --option Acquire::http::Timeout=180 --option APT::Get::Install-Recommends=false --option APT::Get::Install-Suggests=false --yes "$@"
