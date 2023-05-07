73e9b8c92c600f1126c171684816006eafab7a3f
Jaid <jaid.jsx@gmail.com>
2023-05-07 06:41:54 +0200

    Made Python installing optional
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/bin/installPackages.bash
+++ src/bin/installPackages.bash
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
+packages=()
+packages+=(git)
+packages+=(curl)
+packages+=(build-essential)
+packages+=(flex)
+packages+=(texinfo)
+packages+=(gettext)
+packages+=(automake)
+packages+=(pkg-config)
+if [[ -n $pythonPackageVersion ]]; then
+  packages+=("python$pythonPackageVersion")
+  packages+=("python$pythonPackageVersion-dev")
+  packages+=("python$pythonPackageVersion-venv")
+  packages+=("python$pythonPackageVersion-distutils")
+  packages+=("python$pythonPackageVersion-distutils-extra")
+fi
+
 aptGet update
 aptGet upgrade
-aptGet install curl build-essential flex texinfo gettext automake pkg-config python3.12 python3.12-dev python3.12-venv python3.12-distutils python3.12-distutils-extra
+aptGet install "${packages[@]}"
 aptGet autoclean
 aptGet autoremove
 rm --recursive --force /var/log/* /var/lib/apt/lists/* /var/cache/apt/archives/* /usr/share/doc /usr/share/man
