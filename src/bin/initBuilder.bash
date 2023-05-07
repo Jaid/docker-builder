3662b3dc5de81d243d970aa3087b82b106ca766b
Jaid <jaid.jsx@gmail.com>
2023-05-07 06:29:33 +0200

    Improved builder setup
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/bin/initBuilder.bash
+++ src/bin/initBuilder.bash
@@ -1,11 +1,40 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
-# shellcheck disable=SC2154
-addgroup --system --gid "$groupId" "$groupName"
-# shellcheck disable=SC2154
-adduser --disabled-password --gecos '' --uid "$userId" --ingroup "$groupName" --home "$userHome" --no-create-home --shell /bin/bash "$userName"
-mkdir --parents "$userHome/bin"
-mkdir --parents "$userHome/userBin"
-chown --recursive "$userId":"$groupId" "$userHome"
+userId=${userId:-1000}
+userName=${userName:-}
+groupId=${groupId:-1000}
+groupName=${groupName:-$userName}
+userHome=${userHome:-}
+
+existingUser=$(id --name --user "$userId" || true)
+existingGroup=$(id --name --group "$groupId" || true)
+if [[ -n "$existingUser" ]]; then
+  usermod --uid 1001 "$existingUser"
+fi
+if [[ -n "$existingGroup" ]]; then
+  groupmod --gid 1001 "$existingGroup"
+fi
+userAddArguments=()
+if [[ -n "$userHome" ]]; then
+  userAddArguments+=(--home)
+  userAddArguments+=("$userHome")
+  if [[ -d "$userHome" ]]; then
+    userAddArguments+=(--no-create-home)
+  fi
+else
+  userAddArguments+=(--no-create-home)
+fi
+bashPath=$(command -v bash || true)
+if [[ -n "$bashPath" ]]; then
+  userAddArguments+=(--shell)
+  userAddArguments+=("$bashPath")
+fi
+if [[ -n "$groupName" ]]; then
+  groupadd --system --gid "$groupId" "$groupName"
+  userAddArguments+=(--gid)
+  userAddArguments+=("$groupName")
+fi
+useradd --uid "$userId" --home "$userHome" "$userName" "${userAddArguments[@]}"
+
 rm /bin/initBuilder
--- src/bin/installPackages.bash
+++ src/bin/installPackages.bash
@@ -3,7 +3,7 @@ set -o errexit -o pipefail
 
 aptGet update
 aptGet upgrade
-aptGet install curl build-essential flex texinfo gettext automake pkg-config
+aptGet install curl build-essential flex texinfo gettext automake pkg-config python3.12 python3.12-dev python3.12-venv python3.12-distutils python3.12-distutils-extra
 aptGet autoclean
 aptGet autoremove
 rm --recursive --force /var/log/* /var/lib/apt/lists/* /var/cache/apt/archives/* /usr/share/doc /usr/share/man
