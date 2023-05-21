690262ad984c0aa1de9444ec8d747b8f7629ab18
Jaid <jaid.jsx@gmail.com>
2023-05-21 11:04:31 +0200

    Moved cargo index building to a separate building stage
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/userBin/installCargoPackage.bash
+++ src/userBin/installCargoPackage.bash
@@ -4,4 +4,4 @@ set -o errexit -o pipefail
 PATH=$PATH:$HOME/.cargo/bin
 export PATH
 rustTarget=$(rustc -vV | sed -n 's|host: ||p')
-cargo install --target "$rustTarget" --force "$@" --root /
+cargo install--target "$rustTarget" --force "$@" --root /
