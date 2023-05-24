e40893066ea4573b1b8dc28e6b8507b7cf8ec807
Jaid <jaid.jsx@gmail.com>
2023-05-24 22:55:24 +0200

    Improved minifying
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/userBin/installPackagesMinified.bash
+++ src/userBin/installPackagesMinified.bash
@@ -5,4 +5,4 @@ installPackages "$@"
 
 aptGet autoclean
 aptGet autoremove
-rm --recursive --force /var/log/* /var/lib/apt/lists/* /var/cache/apt/archives/* /usr/share/doc /usr/share/man
+rm --recursive --force /var/log/* /var/lib/apt/lists/* /var/cache/apt/archives/* /var/cache/debconf/templates.dat /usr/share/doc /usr/share/man
