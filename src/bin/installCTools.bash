9fa362b317e52ba1a86f828e64baf2cccf793fe2
Jaid <jaid.jsx@gmail.com>
2023-05-24 07:17:32 +0200

    Added ninja-build
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/bin/installCTools.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit -o pipefail
+
+packages=()
+packages+=(build-essential)
+packages+=(flex)
+packages+=(texinfo)
+packages+=(gettext)
+packages+=(automake)
+packages+=(pkg-config)
+packages+=(ninja-build)
+
+installPackages "${packages[@]}"
--- src/bin/installEssentials.bash
+++ src/bin/installEssentials.bash
@@ -4,11 +4,5 @@ set -o errexit -o pipefail
 packages=()
 packages+=(git)
 packages+=(curl)
-packages+=(build-essential)
-packages+=(flex)
-packages+=(texinfo)
-packages+=(gettext)
-packages+=(automake)
-packages+=(pkg-config)
 
 installPackages "${packages[@]}"
