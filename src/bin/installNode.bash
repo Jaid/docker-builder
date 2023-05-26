feec5642fd2910658cb7cae6eb2d2b916dbfde20
Jaid <jaid.jsx@gmail.com>
2023-05-26 04:25:17 +0200

    Improved Node installer
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/bin/installNode.bash
+++ src/bin/installNode.bash
@@ -9,8 +9,17 @@ fi
 safeCurl https://deb.nodesource.com/setup_"$nodeVersion".x | bash -
 installPackagesMinified nodejs
 
-npm install --global npm
+nodeGyp=false
 if [[ -n $pythonPackageVersion ]]; then
+  nodeGyp=true
+fi
+npm install --global npm
+if $nodeGyp; then
   npm install --global node-gyp
 fi
 npm install --global package-field-cli
+node --version
+npm --version
+if $nodeGyp; then
+  node-gyp --version
+fi
