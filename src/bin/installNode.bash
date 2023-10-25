8c73d8f7e7b7849ce750278f36f56d940d796c50
Jaid <6216144+Jaid@users.noreply.github.com>
2023-10-25 10:39:16 +0200

    Updated Node installation, old script is deprecated: https://github.com/nodesource/distributions/blob/b84e91a8f392f73498c26931fac1e635df71e325/README.md?plain=1#L18
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/bin/installNode.bash
+++ src/bin/installNode.bash
@@ -5,8 +5,16 @@ if [[ -z $nodeVersion ]]; then
   printf 'ARG nodeVersion not set, skipping NodeJS installation.\n'
   exit 0
 fi
+NODE_MAJOR="${nodeVersion%%.*}"
+export NODE_MAJOR
 
-safeCurl https://deb.nodesource.com/setup_"$nodeVersion".x | bash -
+keyringsFolder=/etc/apt/keyrings
+if [[ ! -d $keyringsFolder ]]; then
+  printf >&2 'ERROR: %s does not exist.\n' "$keyringsFolder"
+  exit 1
+fi
+safeCurl "https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key" | gpg --dearmor >"$keyringsFolder"/nodesource.gpg
+aptGet update
 installPackagesMinified nodejs
 
 nodeGyp=false
